13417436ccd693d20af0d2da3596c2a2be8f3995
Augusto Rêgo <augustorego2019@gmail.com>
2024-04-16 15:40:53 -0300

    Projeto finalizado mas com alguns bugs a serem corrigidos ainda

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const cartTotal = document.getElementById("cart-total");
 const checkoutBtn = document.getElementById('checkout-btn');  
 const closeModalBtn = document.getElementById('close-modal-btn');
 const cartCounter = document.getElementById('cart-count');
-const addressInput = document.getElementById('addresss');
-const addressWarning = document.getElementById('address-warning');
+const addressInput = document.getElementById('address');
+const addressWarn = document.getElementById('address-warn');
 
 
 let cart = [];
@@ -15,9 +15,10 @@ let cart = [];
 //Função de adicionar ao carrinho 
 
 
-//Abrir o carrinho 
+//Abrir o modal do carrinho 
 cartBtn.addEventListener("click", function() {
     cartModal.style.display = "flex"
+   
 })
 //botão fechar carrinho 
 closeModalBtn.addEventListener("click", function() {
@@ -51,36 +52,144 @@ function addToCart(name, price){
         name,
         price, 
         quantity: 1,
-    })} 
+    })
+} 
 
    updateCartModal();
+
+}
 
     // atualiza carrinho 
 
     function updateCartModal(){
         cartItemsContainer.innerHTML = "";
         let total = 0;
-
+     
         cart.forEach(item => {
             const cartITemsElement = document.createElement("div");
+            cartITemsElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
 
             cartITemsElement.innerHTML =`
-                <div>
+                <div class="flex intems-center justify-between">
                  <div>
-                    <p>${item.name}</p>
-                    <p>${item.quantity}</p>
-                    <p>R$${item.price}</p>
+                    <p calss= "font-medium">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium">R$${item.price.toFixed(2)}</p>
                  </div>
-                
-                    <div>
-                     <buttom>
+                 
+                    <buttom class="remove-from-cart-btn" data-name="${item.name}">
                          Remover
                      </buttom>
-                    </div>
                 </div>      
             `
+            total += item.price * item.quantity
+
             cartItemsContainer.appendChild(cartITemsElement);
         })
+
+        //Calcula o valor total da conta no modal 
+        cartTotal.textContent = total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+
+        //Adiciona a quantidade de intens ao lado do "Veja meu carrinho"
+        cartCounter.innerHTML = cart.length;
+    }
+
+
+//Função para remover item do carrinho 
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+       
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+           item.quantity -= 1; 
+           updateCartModal(); 
+           return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
     
     }
 }
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if (inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+
+})
+
+//finalizar pedido 
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        alert("Restaurante está fechado!")
+        return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    //Enviar pedido para API Whatsapp
+    const cartItems = cart.map((item) =>{
+        return (
+            ` ${item.name} Quantidade: (${item.quantity}) Preço: R$ ${item.price}  |`
+        )
+    })
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "+5521964425118"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+
+
+})
+
+// verificar a hora e manipular o card do horário 
+
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 7 && hora < 22; //hora 
+    
+    // restaurante está aberto 
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+    }else{
+        spanItem.classList.remove("bg-green-600");
+        spanItem.classList.add("bg-red-500");
+    }
